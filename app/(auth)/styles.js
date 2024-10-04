4fea07ede7d6b71ea9048f07ba0293d4981a488f
Eallekors <Erkxalle@gmail.com>
2024-10-04 09:00:56 +0300

    Checkbox

--- app/(auth)/styles.js
+++ app/(auth)/styles.js
@@ -3,7 +3,20 @@ import { StyleSheet,Platform, StatusBar } from "react-native";
 import { colors } from "../../utils/colors"
 
 export const styles = StyleSheet.create({
-    container: { },
+    container: { 
+        padding: 24,
+    },
+    agreeRow: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    agreeText: {
+        color: colors.blue,
+        marginHorizontal: 14
+    },
+    agreeTextBold: {
+        fontWeight: 'bold',
+    },
     AndroidSafeArea: {
         flex: 1,
         backgroundColor: "white",
