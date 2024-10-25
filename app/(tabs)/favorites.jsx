e231f1049acb9ac5b312160821d80d7b74b20f20
Eallekors <Erkxalle@gmail.com>
2024-10-25 09:01:52 +0300

    Create favorites screen

--- app/(tabs)/favorites.jsx
+++ app/(tabs)/favorites.jsx
@@ -1,10 +1,27 @@
-import { SafeAreaView,Text, View } from 'react-native'
-import SafeViewAndroid from '../SafeViewAndroid'
-import React from 'react'
+import { SafeAreaView,Text, View, FlatList } from 'react-native';
+import SafeViewAndroid from '../SafeViewAndroid';
+import React from 'react';
+import { styles } from './styles';
+import { products } from '../../data/products';
+import FavoriteItem from '../../components/FavoriteItem';
+import Header from '../../components/Header';
+
+
 const Favorites = () => {
+  const renderItem = ({item}) => {
+    return (
+      <FavoriteItem 
+        {...item}
+      />
+    )
+  }
   return (
     <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
-      <Text>Favorites</Text>
+      <View style={styles.container}>
+        <Header title="Favorites"/>
+        <FlatList data={products} renderItem={renderItem}
+        keyExtractor={(item) => String(item.id)} />
+      </View>
     </SafeAreaView>
   )
 }
